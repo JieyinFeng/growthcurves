3bf1c059f3cbb314abf91a9aabf2a4737ccb13a5
jyeatman <jyeatman@.uw.edu>
2017-12-14 16:28:31 -0800

    added more models

--- case_cntrl_interaction.m
+++ case_cntrl_interaction.m
@@ -6,5 +6,5 @@ preprocess_revision;
 [stats_int_rf] = fitlme(case_cntrl_data, 'wj_rf ~ int_days_cen*group + (1|record_id) + (int_days_cen - 1|record_id)')
 [stats_int_rf_1] = fitlme(case_cntrl_data(case_cntrl_data.group==1,:), 'wj_rf ~ 1 + int_days_cen + (1|record_id) + (int_days_cen - 1|record_id)')
 [stats_int_rf_0] = fitlme(case_cntrl_data(case_cntrl_data.group==0,:), 'wj_rf ~ 1 + int_days_cen + (1|record_id) + (int_days_cen - 1|record_id)')
-[stats_int_twre] = fitlme(case_cntrl_data, 'twre_index ~ 1 + int_days_cen + int_days_cen*group + (1|record_id) + (int_days_cen - 1|record_id)');
-[stats_int_mcs] = fitlme(case_cntrl_data, 'wj_mcs ~ 1 + int_days_cen + int_days_cen*group + (1|record_id) + (int_days_cen - 1|record_id)');
+[stats_int_twre] = fitlme(case_cntrl_data, 'twre_index ~ 1 + int_days_cen + int_days_cen*group + (1|record_id) + (int_days_cen - 1|record_id)')
+[stats_int_mcs] = fitlme(case_cntrl_data, 'wj_mcs ~ 1 + int_days_cen + int_days_cen*group + (1|record_id) + (int_days_cen - 1|record_id)')
